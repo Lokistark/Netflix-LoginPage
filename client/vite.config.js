2832f5ff6ce0565ace01539dd06028071b9326bb
Lokistark <logeshwarandnc46@gmail.com>
2025-12-07 14:40:28 +0530

    Fix blank GH Page and Danger warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/', // Keep default for Vercel. We will override this via CLI for GitHub Pages.
 })
